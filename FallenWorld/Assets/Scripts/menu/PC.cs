83aabf9780367558c53de93d470431752e659bed
FallenKnightW8 <83452108+FallenKnightW8@users.noreply.github.com>
2022-01-13 13:06:17 +0300

    0.71

---
+++ FallenWorld/Assets/Scripts/menu/PC.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC : MonoBehaviour
+{
+  public GameObject SH;
+public void Shop()
+{
+  SH.SetActive(true);
+  
+}
+
+
+}
