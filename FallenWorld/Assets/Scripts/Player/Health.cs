960a841939516467ffe0e24cca99f58f98c953f3
FallenKnightW8 <83452108+FallenKnightW8@users.noreply.github.com>
2023-03-15 11:09:11 +0300

    Update Health.cs

--- FallenWorld/Assets/Scripts/Player/Health.cs
+++ FallenWorld/Assets/Scripts/Player/Health.cs
@@ -1,11 +1,3 @@
-﻿/// <summary>
-/// Health.cs
-/// Author: MutantGopher
-/// This is a sample health script.  If you use a different script for health,
-/// make sure that it is called "Health".  If it is not, you may need to edit code
-/// referencing the Health component from other scripts
-/// </summary>
-
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
