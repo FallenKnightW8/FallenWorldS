64a9e7ba26b6522232a24d209effa1bff7c89fcd
FallenKnightW8 <83452108+FallenKnightW8@users.noreply.github.com>
2023-07-02 01:45:22 +0300

    0,745
    1

--- FallenWorld/Assets/Scripts/Enemy/Ranck.cs
+++ FallenWorld/Assets/Scripts/Enemy/Ranck.cs
@@ -7,51 +7,56 @@ using UnityEngine;
 
 public class Ranck : MonoBehaviour
 {
+    [SerializeField] private bool IsBoss = false;
     [SerializeField]private int Rank = 1;   //lvl of enemy treath
     public Health RankH;                    //Url to chage Hp
     public EnemyAiTutorial Damag;           //Url to Chage Dm
     public Health MFD;                      //URl o Chage MFD
 
     public GameObject[] EnemyModels;        //or prefabs?
-
+    private Mission GetTheRangOfMission;
+    private int MultiplerOutRang;
     public Mission countOFEnemis;           //The First Type of mission
     void Start()
     {
         countOFEnemis = GameObject.Find("MissionManager").GetComponent<Mission>();
         countOFEnemis.NeededVaule +=1;
-        //Rank = Random.Range(1,5);
+        GetTheRangOfMission = GameObject.Find("MissionManager").GetComponent<Mission>();
+        MultiplerOutRang = GetTheRangOfMission.RangOfM;
+        if (!IsBoss)
+            Rank = Random.Range(1,5);
         //      maxHealth = GetComponent<Health>().maxHealth;
         switch (Rank)
         {
             case 1: //pistol,PP
                 RankH.startingHealth +=50;
                 RankH.maxHealth += 50;
-                MFD.MoneyForDead *= 2;
+                MFD.MoneyForDead *= 2 * MultiplerOutRang;
                 break;
 //spawn enemy
 
             case 2://shield,Rifle,shotgan
                 RankH.maxHealth += 100;
                 RankH.startingHealth +=100;
-                MFD.MoneyForDead = 10;
+                MFD.MoneyForDead *= 2 * MultiplerOutRang;
                 break;
 
             case 3://long rifle,medic,shotgan
                 RankH.maxHealth += 150;
                 RankH.startingHealth +=150;
-                MFD.MoneyForDead = 30;
+                MFD.MoneyForDead *= 3 * MultiplerOutRang;
                 break;
 
             case 4:
                 RankH.maxHealth += 200;
                 RankH.startingHealth += 200;
-                MFD.MoneyForDead = 60;
+                MFD.MoneyForDead *= 3 * MultiplerOutRang;
                 break;
 
             case 5://bosses,jager,fireman
                 RankH.maxHealth += 300;
                 RankH.startingHealth += 300;
-                MFD.MoneyForDead = 120;
+                MFD.MoneyForDead *= 5 * MultiplerOutRang;
                 break;
 
             default:
--- FallenWorld/Assets/Scripts/missions/Mission.cs
+++ FallenWorld/Assets/Scripts/missions/Mission.cs
@@ -11,7 +11,7 @@ public class Mission : MonoBehaviour
     [Header("RanckOf Mission")]
     private int countOFmissionReady; //counter of value how mani missions have bin complite
     private int TypeOfMission;      //Random of type of target for the player//0 - kill everyware,1 - steal to any summ,2 - open the final voult,3 - destroy to any summ 
-    private int RangOfM = 1;        //every 5 complit mission = nwe rang? the final rang is 5? when final boss
+    public int RangOfM = 1;        //every 5 complit mission = nwe rang? the final rang is 5? when final boss
     public bool BossOfRangKilled = false;
 
     private FirstPersonCharacter Locks;
